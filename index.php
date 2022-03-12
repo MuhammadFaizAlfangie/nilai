551f164134cc62da6be89035faa09625802b10e4
Muhammad Faiz Alfangie <muhammadfaizalfangie@gmail.com>
2022-03-12 13:55:58 +0700

    upload awal

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Nilai</title>
+</head>
+<body>
+    <!-- Muhammad Faiz Alfangie-Web Programming-->
+    <h1> Data Nilai Siswa </h1>
+    <a href="kelas">Kelas</a> | 
+    <a href="siswa">Siswa</a>
+</body>
+</html>
